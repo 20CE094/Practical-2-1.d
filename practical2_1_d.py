91edf681c5f646de5795e80aa458484a7d8cc73e
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:33:34 +0530

    Add files via upload

---
+++ practical2_1_d.py
@@ -0,0 +1,11 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-1.d.git
+#Aim:  Write a Python script to add a key to a dictionary.
+a = {1:1,2:2}
+print(a)
+#adding a key inside a existing dictionary
+a.update({3:3})
+a.update({5:8})
+#printing updated dictionary
+print(a)
